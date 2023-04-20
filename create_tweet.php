bb491ff0cebdd84fd5f390727fe7e42a9c4f5861
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 10:54:23 -0400

    Update create_tweet.php

--- create_tweet.php
+++ create_tweet.php
@@ -1,1 +1,13 @@
 <?php
+$headers = array(
+    "Content-Type: application/json",
+    "Authorization: Bearer ".$config["open_ai_key"]
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $config["open_ai_url"]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+
+
+$output = curl_exec($ch);

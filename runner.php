2d5bf4ab12bd77e5b996ea76bede309eea299289
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 10:12:27 -0400

    Update runner.php

--- runner.php
+++ runner.php
@@ -5,3 +5,11 @@ if( php_sapi_name() !== 'cli' ){die();}
 //endRemoveIf(development)
 
 require_once "config.php";
+
+$last_tweet = file_get_contents($config["last_tweet_file_location"]);
+
+$gun_db = file_get_contents($config["gun_db_url"]); 
+if(!$gun_db) {
+  die();
+}
+$gun_db = json_decode($gun_db); 

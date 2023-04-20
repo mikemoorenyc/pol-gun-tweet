8bf486666bdc91aa48a90e4725871b88d8960bd7
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 10:13:38 -0400

    Update runner.php

--- runner.php
+++ runner.php
@@ -9,6 +9,7 @@ require_once "config.php";
 $last_tweet = file_get_contents($config["last_tweet_file_location"]);
 
 $gun_db = file_get_contents($config["gun_db_url"]); 
+//Couldn't get gun db
 if(!$gun_db) {
   die();
 }

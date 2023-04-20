c605f8ac441a85a9929ccfe10df036266331158b
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 11:15:06 -0400

    Update send_tweet.php

--- send_tweet.php
+++ send_tweet.php
@@ -1,3 +1,18 @@
 <?php
 
 
+$settings = array(
+    'oauth_access_token' => $config["twitter_token"],
+    'oauth_access_token_secret' => $config["twitter_token_secret"],
+    'consumer_key' => $config["twitter_consumer_key"],
+    'consumer_secret' => $config["twitter_secret"]
+);
+$twitterNew = new TwitterAPIExchange($settings);
+			$postfields = array(
+				'status' => $tweet_body
+);
+
+$postedTweet = $twitterNew->buildOauth('https://api.twitter.com/1.1/statuses/update.json', 'POST')->setPostfields($postfields)->performRequest();
+
+
+?>

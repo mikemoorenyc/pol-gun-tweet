5fa40a18e37992bae5594fe239aa0decf6ec7bab
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 11:09:16 -0400

    Update create_tweet.php

--- create_tweet.php
+++ create_tweet.php
@@ -1,4 +1,12 @@
 <?php
+
+$prompt = $config["prompt"];
+
+$prompt = str_replace("%location%",$location, $prompt);  
+
+$data = $config["data_package"];
+$data["prompt"] = $prompt;
+
 $headers = array(
     "Content-Type: application/json",
     "Authorization: Bearer ".$config["open_ai_key"]
@@ -8,6 +16,13 @@ curl_setopt($ch, CURLOPT_URL, $config["open_ai_url"]);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-
-
 $output = curl_exec($ch);
+if ($output === FALSE) {
+ echo "cURL Error: " . curl_error($ch);
+ die();
+}
+$response = json_decode($output, true);
+$tweet_body = $response["choices"][0]["text"];
+if($tweet_body) {
+    die();
+}

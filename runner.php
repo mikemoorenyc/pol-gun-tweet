7c01c42e141b5b18141575f74c3e26faa2aa85e5
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 10:40:17 -0400

    Update runner.php

--- runner.php
+++ runner.php
@@ -7,9 +7,7 @@ if( php_sapi_name() !== 'cli' ){die();}
 require_once('config.php');
 require_once('TwitterAPIExchange.php');
 
-/*** MAKE URL WITH TODAY'S YEAR ****/
 
-$last_shooting = file_get_contents($config["last_shooting_file_location"]);
 
 require_once("check_new.php");
 

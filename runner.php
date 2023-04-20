7b20c60a0f5e1e948e5974d80ef0e770dfb94aa0
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 10:36:40 -0400

    Update runner.php

--- runner.php
+++ runner.php
@@ -5,8 +5,12 @@ if( php_sapi_name() !== 'cli' ){die();}
 //endRemoveIf(development)
 
 require_once('config.php');
+require_once('TwitterAPIExchange.php');
 
-$last_tweet = file_get_contents($config["last_tweet_file_location"]);
+/*** MAKE URL WITH TODAY'S YEAR ****/
+
+$last_shooting = file_get_contents($config["last_shooting_file_location"]);
+
 
 $gun_db = file_get_contents($config["gun_db_url"]); 
 //Couldn't get gun db
@@ -14,3 +18,16 @@ if(!$gun_db) {
   die();
 }
 $gun_db = json_decode($gun_db); 
+$new_shooting = $gun_db[0];
+if($gun_db[0]->date == $last_shooting) {
+  // KILL IF IT'S OLD
+  
+  //removeIf(!development)
+  die();
+  //endRemoveIf(!development)
+  $new_shooting = $gun_db[rand(0, count($gun_db) - 1)];
+}
+
+
+
+?>

fdae3223f8820f998b0f7c3039b3f16e26ece939
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2023-04-20 10:48:19 -0400

    Update check_new.php

--- check_new.php
+++ check_new.php
@@ -1,4 +1,8 @@
 <?php
+/*** MAKE URL WITH TODAY'S YEAR ****/
+
+$last_shooting = file_get_contents($config["last_shooting_file_location"]);
+
 $gun_db = file_get_contents($config["gun_db_url"]); 
 //Couldn't get gun db
 if(!$gun_db) {
@@ -6,11 +10,19 @@ if(!$gun_db) {
 }
 $gun_db = json_decode($gun_db); 
 $new_shooting = $gun_db[0];
+$rand = false;
 if($gun_db[0]->date == $last_shooting) {
   // KILL IF IT'S OLD
   
-  //removeIf(!development)
+  //removeIf(development)
   die();
-  //endRemoveIf(!development)
-  $new_shooting = $gun_db[rand(0, count($gun_db) - 1)];
+  //endRemoveIf(development)
+  $rand = true;
+  $new_shooting = $gun_db[rand(0, count($gun_db) - 1)]; 
+  
 }
+if(!$rand) {
+  file_put_contents($config["last_shooting_file_location"],$new_shooting->date);
+}
+
+
